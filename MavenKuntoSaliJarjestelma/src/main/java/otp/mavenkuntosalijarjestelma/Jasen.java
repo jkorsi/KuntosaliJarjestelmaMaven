5606d682d462f88188472803681ccaf1a064228a
Antti Käyhkö <antti859@gmail.com>
2018-09-17 12:06:23 +0300

    Jasen.java luokan hibernate annotaatiot laitettu

--- MavenKuntoSaliJarjestelma/src/main/java/otp/mavenkuntosalijarjestelma/Jasen.java
+++ MavenKuntoSaliJarjestelma/src/main/java/otp/mavenkuntosalijarjestelma/Jasen.java
@@ -1,15 +1,67 @@
 package otp.mavenkuntosalijarjestelma;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "jasenet")
 public abstract class Jasen {
+    
+    @Column(name = "nimi")
+    private String nimi;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "jasen_ID")
+    private int jasenNro;
+    
+    @Column(name = "maksutapa")
+    private String maksuTapa;
+    
+    @Column(name = "jasenyysvoimassa")
+    private boolean onkoJasenyysVoimassa;
 
-	private String nimi;
+    public Jasen() {
 
-	private int jasenNro;
+    }
+    
+    public String getNimi() {
+        return nimi;
+    }
 
-	private String maksuTapa;
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
 
-	private boolean onkoJasenyysVoimassa;
+    
+    public int getJasenNro() {
+        return jasenNro;
+    }
 
-	private Jasenlista jasenlista;
+    public void setJasenNro(int jasenNro) {
+        this.jasenNro = jasenNro;
+    }
+    
+    public String getMaksuTapa() {
+        return maksuTapa;
+    }
+
+    public void setMaksuTapa(String maksuTapa) {
+        this.maksuTapa = maksuTapa;
+    }
+
+    public boolean isOnkoJasenyysVoimassa() {
+        return onkoJasenyysVoimassa;
+    }
+
+    public void setOnkoJasenyysVoimassa(boolean onkoJasenyysVoimassa) {
+        this.onkoJasenyysVoimassa = onkoJasenyysVoimassa;
+    }
+
 
 }
