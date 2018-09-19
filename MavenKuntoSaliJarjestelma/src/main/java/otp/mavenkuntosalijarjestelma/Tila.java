cb25ecef4dbe5e833da66d208725c4dcb93e812e
JuhoSuni <juhosu@metropolia.fi>
2018-09-19 14:12:20 +0300

    Poistettu jäsenlista luokka.

--- MavenKuntoSaliJarjestelma/src/main/java/otp/mavenkuntosalijarjestelma/Tila.java
+++ MavenKuntoSaliJarjestelma/src/main/java/otp/mavenkuntosalijarjestelma/Tila.java
@@ -1,8 +1,12 @@
 package otp.mavenkuntosalijarjestelma;
 
+import javax.persistence.Id;
+
 public class Tila {
 
-    //Spam 
+    @Id
+    private int tilaID;
+
     private int suosituinKuntoSaliLaite;
 
     private int laitteidenMaara;
@@ -10,5 +14,56 @@ public class Tila {
     private int tilanKulut;
 
     private Kuntosalilaite[] kuntosalilaite;
+
+    public Tila() {
+    }
+
+    public Tila(int tilaID, int suosituinKuntoSaliLaite, int laitteidenMaara, int tilanKulut, Kuntosalilaite[] kuntosalilaite) {
+        this.tilaID = tilaID;
+        this.suosituinKuntoSaliLaite = suosituinKuntoSaliLaite;
+        this.laitteidenMaara = laitteidenMaara;
+        this.tilanKulut = tilanKulut;
+        this.kuntosalilaite = kuntosalilaite;
+    }
+
+    public int getTilaID() {
+        return tilaID;
+    }
+
+    public void setTilaID(int tilaID) {
+        this.tilaID = tilaID;
+    }
+
+    public int getSuosituinKuntoSaliLaite() {
+        return suosituinKuntoSaliLaite;
+    }
+
+    public void setSuosituinKuntoSaliLaite(int suosituinKuntoSaliLaite) {
+        this.suosituinKuntoSaliLaite = suosituinKuntoSaliLaite;
+    }
+
+    public int getLaitteidenMaara() {
+        return laitteidenMaara;
+    }
+
+    public void setLaitteidenMaara(int laitteidenMaara) {
+        this.laitteidenMaara = laitteidenMaara;
+    }
+
+    public int getTilanKulut() {
+        return tilanKulut;
+    }
+
+    public void setTilanKulut(int tilanKulut) {
+        this.tilanKulut = tilanKulut;
+    }
+
+    public Kuntosalilaite[] getKuntosalilaite() {
+        return kuntosalilaite;
+    }
+
+    public void setKuntosalilaite(Kuntosalilaite[] kuntosalilaite) {
+        this.kuntosalilaite = kuntosalilaite;
+    }
 
 }
