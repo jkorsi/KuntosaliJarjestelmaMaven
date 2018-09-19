3f498fb7a662430018aeffdb2272f512efd2953d
Antti Käyhkö <antti859@gmail.com>
2018-09-19 13:41:25 +0300

    Error jasenmaksy buggy no touchy plz

--- MavenKuntoSaliJarjestelma/src/main/java/otp/mavenkuntosalijarjestelma/jasenMaksu.java
+++ MavenKuntoSaliJarjestelma/src/main/java/otp/mavenkuntosalijarjestelma/jasenMaksu.java
@@ -1,11 +1,32 @@
 package otp.mavenkuntosalijarjestelma;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class jasenMaksu {
-
+    
+    @Column(name="kkhinta")
     private int kkHinta;
-
+    @Column(name="kertahinta")
     private int kertaHinta;
+    
+    @Id
+    private int maksutapahtumaID
+    
+    @ManyToOne
+    @JoinColumn(name="jasen_ID")
+    private int jasenID;
 
-    private Maksujarjestelma maksujarjestelma;
+    public int getMaksutapahtumaID() {
+        return maksutapahtumaID;
+    }
+
+    public void setMaksutapahtumaID(int maksutapahtumaID) {
+        this.maksutapahtumaID = maksutapahtumaID;
+    }
 
 }
